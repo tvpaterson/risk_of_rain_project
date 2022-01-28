99667506124980e8460d3ad4f01cb5b79a57324c
tvpaterson <taylorpaterson7@gmail.com>
2022-01-28 14:24:45 +0000

    added base html file

--- console.py
+++ console.py
@@ -1,7 +1,4 @@
 import pdb
-
-
-import pdb
 from models.character import Character
 from models.item import Item
 import repositories.character_repository as character_repository
@@ -10,11 +7,7 @@ import repositories.item_repository as item_repository
 # character_repository.delete_all()
 # item_repository.delete_all()
 
-item7 = Item('Red Whip', 'Sprint faster when out of combat')
-item_repository.save(item7)
 
-
-print(item_repository.save(item7))
 
 
 pdb.set_trace()
